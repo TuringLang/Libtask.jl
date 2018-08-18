a4da8cf4e601563371fc45c0beee09dc13cbb5cb
Hong Ge <hg344@cam.ac.uk>
2018-08-18 22:17:42 +0100

    Update task.c

--- task.c
+++ task.c
@@ -11,7 +11,7 @@ jl_task_t *jl_clone_task(jl_task_t *t)
     //jl_task_t *newt = (jl_task_t*)jl_gc_alloc(ptls, sizeof(jl_task_t),
     //                                       jl_task_type);   // jl_gc_alloc is not exported.
     jl_task_t *newt = (jl_task_t*)jl_gc_allocobj(sizeof(jl_task_t)); // implementation a.
-    //jl_task_t *newt = (jl_task_t*)jl_new_task(t->start, t->ssize); //  Not efficient as a.
+    //jl_task_t *newt = (jl_task_t*)jl_new_task(t->start, t->ssize); //  Less efficient than a.
     memset(newt, 0, sizeof(jl_task_t));
     jl_set_typeof(newt, jl_task_type);
     newt->stkbuf = NULL;
@@ -19,8 +19,6 @@ jl_task_t *jl_clone_task(jl_task_t *t)
     JL_GC_PUSH1(&newt);
 
     newt->parent = ptls->current_task;
-    // newt->parent = t->parent;
-    // newt->last = t->last;
     newt->current_module = t->current_module;
     newt->state = t->state;
     newt->start = t->start;
@@ -44,7 +42,7 @@ jl_task_t *jl_clone_task(jl_task_t *t)
 //#ifdef COPY_STACKS
     if (t->stkbuf){
         newt->ssize = t->ssize;  // size of saved piece
-        // newt->stkbuf = allocb(t->bufsz); // needs to be allocb(t->bufsz)
+        // newt->stkbuf = allocb(t->bufsz); 
         // newt->bufsz = t->bufsz;
         // memcpy(newt->stkbuf, t->stkbuf, t->bufsz);
         // workaround, newt and t will get new stkbuf when savestack is called.
