9238eb3bb7bf46ed9f9d9c5576d80e82374fe80d
KDr2 <zhuo@hexoasis.com>
2019-06-25 11:40:04 +0800

    rebuild while deps.jl is not generated correctly

--- src/Libtask.jl
+++ src/Libtask.jl
@@ -2,6 +2,11 @@ module Libtask
 
 export CTask, consume, produce, TArray, get, tzeros, tfill, TRef
 
+if !isfile(joinpath((@__DIR__) |> dirname, "deps/desp.jl"))
+    import Pkg
+    Pkg.build("Libtask")
+end
+
 include("../deps/deps.jl"); check_deps();
 include("taskcopy.jl")
 include("tarray.jl")
