5e9d7261e6a5c9beb30e5f8ec01f474a11e6a73e
KDr2 <zhuo@hexoasis.com>
2020-04-24 15:17:36 +0800

    COW: improve data reading

--- src/cow.jl
+++ src/cow.jl
@@ -49,6 +49,28 @@ function copy_object(obj)
 end
 
 """
+    copy_for_reading(obj)
+
+If current task owns a copy of `obj`, returns the copy, else returns
+obj itself.
+
+"""
+function copy_for_reading(obj)
+    ct = _current_task()
+    ct.storage === nothing && return obj
+    haskey(ct.storage, :_cow_asset) || return obj
+    haskey(ct.storage, :_cow_copylog) || return obj
+
+    haskey(ct.storage[:_cow_asset], obj) && (return obj) # own this obj
+    if haskey(ct.storage[:_cow_copylog], objectid(obj)) # had copied this obj
+        ret = ct.storage[:_cow_copylog][objectid(obj)]
+        ct.storage[:_cow_asset][ret] = true
+        return ret
+    end
+    return obj
+end
+
+"""
     maybe_copy(x)
 
 Copy an object when the COW mechanism requires to make a copy. If you
@@ -71,13 +93,13 @@ maybe_copy(x::AbstractDict) = begin
 end
 
 # (func_name => mutated_arg_pos)
-const MUTATING_OPS = Dict{Symbol, Int}(
-    :setindex! => 1,
-    :push! => 1, :pushfirst! => 1,
-    :pop! => 1, :popfirst! => 1,
-    :append! => 1,
-    :delete! => 1, :deleteat! => 1,
-    :setdiff! => 1,
+const MUTATING_OPS = Dict{Any, Vector{Int}}(
+    :setindex! => [1],
+    :push! => [1], :pushfirst! => [1],
+    :pop! => [1], :popfirst! => [1],
+    :append! => [1],
+    :delete! => [1], :deleteat! => [1],
+    :setdiff! => [1],
 )
 
 """
@@ -85,93 +107,102 @@ const MUTATING_OPS = Dict{Symbol, Int}(
 
 Predicate if an expression is a mutating operation.
 If true, returns `ture` and the position of the mutated argument;
-else returns `(false, 0)`.
+else returns `(false, [0])`.
 
 Examples:
 
-- `push!(a, v)` -> (true, 1)
-- `print(a)` -> (false, 0)
+- `push!(a, v)` -> (true, [1])
+- `print(a)` -> (false, [0])
 
 """
 function mutating(e)
-    isa(e, Expr) || return (false, 0)
-    e.head == :call || return (false, 0)
-    if isa(e.args[1], GlobalRef) && haskey(MUTATING_OPS, e.args[1].name)
-        return (true, MUTATING_OPS[e.args[1].name])
+    isa(e, Expr) || return (false, [0])
+    e.head == :call || return (false, [0])
+    (args, offset) = if e.args[1] === IRTools.self
+        (e.args[2:end], 1)
+    else
+        (e.args[1:end], 0)
     end
 
-    # IRTools.Statement(:((IRTools.Self())(Main.push!, %2, 1)), Any, 2)
-    if (e.args[1] === IRTools.self) && isa(e.args[2], GlobalRef) &&
-        haskey(MUTATING_OPS, e.args[2].name)
-        return (true, MUTATING_OPS[e.args[2].name] + 1)
-    end
-    return (false, 0)
-end
+    #@show args[1]
 
-function _successors(b::IRTools.Block, accu::Vector{Int64})
-    succs = IRTools.successors(b)
-    ids = map(x -> x.id, succs)
-    issubset(ids, accu) && return
-    for blk in succs
-        push!(accu, blk.id)
-        _successors(blk, accu)
+    if isa(args[1], GlobalRef)
+        if haskey(MUTATING_OPS, args[1])
+            return (true, MUTATING_OPS[args[1]] .+ offset)
+        elseif haskey(MUTATING_OPS, args[1].name)
+            return (true, MUTATING_OPS[args[1].name] .+ offset)
+        end
     end
-end
 
-function _successors(b::IRTools.Block)
-    ret = Int64[]
-    _successors(b, ret)
-    return ret
+    if isa(args[1], IRTools.Variable) # call to a closure
+        # @show "closure called!"
+        idx = collect(1:length(args) - 1)
+        return (true, idx .+ offset)
+    end
+    return (false, [0])
 end
 
 
 """
-    find_mutating_blocks(ir::IRTools.IR)
+    find_mutated_vars(ir::IRTools.IR)
 
-Find all variables that are mutated in the IR, and all the blocks in
-which a certain variable is modified.
+Find all variables that are mutated in the given IR.
 
 """
-function find_mutating_blocks(ir::IRTools.IR)
-    vars_to_blocks = Dict{IRTools.Variable, Vector{Int64}}()
-    for blk in IRTools.blocks(ir)
-        for (v, st) in blk
-            mut, mpos = mutating(st.expr)
-            mut || continue
+function find_mutated_vars(ir::IRTools.IR)
+    vars = Vector{IRTools.Variable}()
+    for (v, st) in ir
+        mut, mpositions = mutating(st.expr)
+        mut || continue
+        for mpos in mpositions
             # mpos = 1 when push!(...), mpos = 2 when self(push!, ...)
             isa(st.expr.args[mpos + 1], IRTools.Variable) || continue
             mv = st.expr.args[mpos + 1]
-            if haskey(vars_to_blocks, mv)
-                push!(vars_to_blocks[mv], blk.id)
-            else
-                vars_to_blocks[mv] = [blk.id]
-            end
-            push!(vars_to_blocks[mv], _successors(blk)...)
+            push!(vars, mv)
         end
     end
 
-    for (v, blk_ids) in vars_to_blocks
-        unique!(blk_ids)
-    end
-
-    return vars_to_blocks
+    return vars
 end
 
-function insert_copy_for_var(ir::IRTools.IR, var, blk_ids::Vector{Int64})
-    mutate_instructions = Dict{IRTools.Variable, IRTools.Variable}()
+"""
+    insert_copy_for_var(ir::IRTools.IR, var)
 
-    for blk_id in blk_ids
-        blk = IRTools.block(ir, blk_id)
-        for (v, st) in blk
-            isa(st.expr, Expr) || continue
-            (var in st.expr.args) || continue
-            rk = insert!(blk, v, IRTools.xcall(Libtask, :maybe_copy, var))
-            for i in 1:length(st.expr.args)
-                st.expr.args[i] == var && (st.expr.args[i] = rk)
-            end
+For a given variable:
+- find the expressions mutate it, insert a `maybe_copy` before them;
+- find the expressions read it, insert a `copy_for_reading` before them.
+
+"""
+function insert_copy_for_var(ir::IRTools.IR, var)
+    for (v, st) in ir
+        isa(st.expr, Expr) || continue
+        (var in st.expr.args) || continue
+        mut, mpositions = mutating(st.expr)
+        if mut # write
+            rk = insert!(ir, v, IRTools.xcall(Libtask, :maybe_copy, var))
+        else # read
+            rk = insert!(ir, v, IRTools.xcall(Libtask, :copy_for_reading, var))
+        end
+        for i in 1:length(st.expr.args)
+            st.expr.args[i] == var && (st.expr.args[i] = rk)
         end
     end
 
+    return ir
+end
+
+
+"""
+    insert_copy_stage_2(ir::IR)
+
+For every muated variable in the given IR, do `insert_copy_for_var`.
+
+"""
+function insert_copy_stage_2(ir::IRTools.IR)
+    mutated_vars = find_mutated_vars(ir)
+    for var in mutated_vars
+        insert_copy_for_var(ir, var)
+    end
     return ir
 end
 
@@ -181,7 +212,6 @@ end
 
 If a variable is mutated in an IR, we find its first occurrence, then
 insert a `maybe_copy` after that occurrence.
-
 This is for handle read operations after write opertions.
 
 """
@@ -190,11 +220,13 @@ function insert_copy_stage_1(ir::IRTools.IR)
     replacements = Dict{IRTools.Variable, IRTools.Variable}()
 
     for (v, st) in ir
-        mut, mpos = mutating(st.expr)
+        mut, mpositions = mutating(st.expr)
         mut || continue
-        # mpos = 1 when push!(...), mpos = 2 when self(push!, ...)
-        if isa(st.expr.args[mpos + 1], IRTools.Variable)
-            mutate_vars[st.expr.args[mpos + 1]] = true
+        for mpos in mpositions
+            # mpos = 1 when push!(...), mpos = 2 when self(push!, ...)
+            if isa(st.expr.args[mpos + 1], IRTools.Variable)
+                mutate_vars[st.expr.args[mpos + 1]] = true
+            end
         end
     end
 
@@ -217,22 +249,23 @@ function insert_copy_stage_1(ir::IRTools.IR)
     return ir_new
 end
 
-
-"""
-    insert_copy_stage_2(ir::IR)
-
-Find every mutating expression, insert a `maybe_copy` before it.
-
-"""
-function insert_copy_stage_2(ir::IRTools.IR)
-    mv_blocks = find_mutating_blocks(ir)
-    for (var, blk_ids) in mv_blocks
-        insert_copy_for_var(ir, var, blk_ids)
-    end
-    return ir
-end
-
 insert_copy(ir) = ir |> insert_copy_stage_1 |> insert_copy_stage_2
+
+"""
+    check_func_mutating(func::GlobalRef, ir::IRTools.IR)
+
+Check if a function mutates its arguments, and if it does, mutates
+which ones.
+
+"""
+
+function check_func_mutating(func, ir::IRTools.IR)
+    # @show "cf", ir
+    vars = find_mutated_vars(ir)
+    args = IRTools.arguments(ir)
+    positions = [x for x in indexin(vars, args) if x !== nothing]
+    isempty(positions) || (MUTATING_OPS[func] = positions .- 1)
+end
 
 function recurse_no_builtin!(ir, to = IRTools.self)
     for (x, st) in ir
@@ -249,6 +282,10 @@ end
 IRTools.@dynamo function cow(a...)
     ir = IRTools.IR(a...)
     ir === nothing && return
+    # a[1] is a
+    # - GlobalRef for global + named function, or
+    # - a subtype of Function for closure/anonymous function
+    isa(a[1], GlobalRef) && check_func_mutating(a[1], ir)
     recurse_no_builtin!(ir)
     ir = insert_copy(ir)
     return ir
@@ -304,10 +341,18 @@ end
 
 # debug
 function cmp_cow_ir(func, args...)
-    ir = IRTools.@code_ir func(args...)
-    print("\nIR before transformation --------\n")
-    print(ir)
-    ir = insert_copy(ir)
-    print("\nIR after transformation --------\n")
-    print(ir)
+    ir_00 = IRTools.@code_ir func(args...)
+    print("\nIR [Original] -------------\n")
+    print(ir_00)
+    ir_01 = insert_copy_stage_1(ir_00)
+    print("\nIR [Stage 1]  -------------\n")
+    print(ir_01)
+    ir_12 = insert_copy_stage_2(ir_01)
+    print("\nIR [Stage 2]  -------------\n")
+    print(ir_12)
+    ir_00 = IRTools.@code_ir func(args...)
+    ir_02 = insert_copy_stage_2(ir_00)
+    print("\nIR [Only Stage 2]  --------\n")
+    print(ir_02)
+    print("\n")
 end
