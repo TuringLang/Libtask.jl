6047ad167739f51e8cb3ffc04d8bbe4c8de9d6da
David Widmann <dev+git@devmotion.de>
2020-10-09 17:43:25 +0200

    Use Libtask_jll

--- src/Libtask.jl
+++ src/Libtask.jl
@@ -1,17 +1,8 @@
 module Libtask
 
+using Libtask_jll
+
 export CTask, consume, produce, TArray, get, tzeros, tfill, TRef
-
-# Try to load the binary dependency
-const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
-if !isfile(depsjl_path)
-    error("Libtask is not properly installed. Please run `import Pkg; Pkg.build(\"Libtask\")`")
-end
-include(depsjl_path)
-
-function __init__()
-    check_deps()
-end
 
 @static if VERSION < v"1.0.9999" # (-, v1.1)
     const libtask = libtask_v1_0
