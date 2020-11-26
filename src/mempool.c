9f14ca40ba4165a83c666c35aeb5a77ec07105dd
KDr2 <zhuo@hexoasis.com>
2020-11-26 05:33:04 +0000

    try to alloca mem on stack and copy data onto it

--- src/mempool.c
+++ src/mempool.c
@@ -4,9 +4,32 @@
 
 #include "julia.h"
 
+JL_DLLEXPORT void jl_obj_inspect(jl_value_t *obj)
+{
+    int i = 0;
+    jl_ptls_t ptls = jl_get_ptls_states();
+    jl_task_t *t = (jl_task_t*)ptls->current_task;
+    char *stackbase = (char*)ptls->stackbase;
+
+    printf("stacksize        = %ld, %ld\n", ptls->stacksize, t->bufsz);
+    printf("offset of i      = %ld\n", (char*)(&i) - stackbase);
+    printf("offset of i      = %ld\n", (char*)(&i) - (char*)t->stkbuf);
+    printf("offset of obj    = %ld\n", (char*)obj - stackbase);
+    printf("offset of obj    = %ld\n", (char*)obj - (char*)t->stkbuf);
+    if(jl_is_int64(obj)) {
+        printf("int value        = %ld\n", *(long*)obj);
+    }
+    printf("-------------\n");
+}
+
 
 JL_DLLEXPORT jl_array_t *jl_array_inspect(jl_array_t *ary, void *new_data)
 {
+
+    jl_ptls_t ptls = jl_get_ptls_states();
+    //char *frame_addr = (char*)((uintptr_t)jl_get_frame_addr() & ~15);
+    char *stackbase = (char*)ptls->stackbase;
+
     printf("flags.how = %d\n", ary->flags.how);
     printf("flags.ndims = %d\n", ary->flags.ndims);
     printf("flags.pooled = %d\n", ary->flags.pooled);
@@ -14,10 +37,36 @@ JL_DLLEXPORT jl_array_t *jl_array_inspect(jl_array_t *ary, void *new_data)
     printf("flags.hasptr = %d\n", ary->flags.hasptr);
     printf("flags.isshared = %d\n", ary->flags.isshared);
     printf("flags.isaligned = %d\n", ary->flags.isaligned);
+    printf("array ptr = %p\n", ary);
     printf("data ptr = %p\n", ary->data);
 
+    printf("offset = %ld\n", stackbase - (char*)ary);
     if (new_data) {
         ary->data = new_data;
     }
+    printf("-------------\n");
     return ary;
+}
+
+JL_DLLEXPORT jl_value_t * jl_icaller(jl_value_t *fun)
+{
+    int i = 0;
+    printf("position of i 1: %p\n", &i);
+    printf("s = %ld\n", sizeof(jl_array_t));
+    void *buf = alloca(2048);
+    printf("position of buf: %p\n", buf);
+    jl_function_t* func = (jl_function_t*)fun;
+    jl_value_t *argument = jl_box_int64((int64_t)buf);
+    jl_value_t *ret = jl_call1(func, argument);
+    return ret;
+}
+
+JL_DLLEXPORT jl_array_t* jl_copy_array_to_stack(jl_array_t *ary, void *dest)
+{
+    int i = 0;
+    printf("position of i 2: %p\n", &i);
+    printf("position of buf: %p\n", dest);
+    memcpy(dest, (void*)ary, sizeof(jl_array_t));
+    printf("------\n");
+    return (jl_array_t*)(dest);
 }
--- src/mempool.jl
+++ src/mempool.jl
@@ -3,26 +3,76 @@ using Libdl
 
 @show lib = Libdl.dlopen_e("./mempool.so")
 @show inspector = dlsym(lib, :jl_array_inspect)
+@show obj_inspector = dlsym(lib, :jl_obj_inspect)
+@show icaller = dlsym(lib, :jl_icaller)
+@show a_copy = dlsym(lib, :jl_copy_array_to_stack)
+
+struct S
+    i::Int
+end
+
+function t1()
+    a0 = [1 2; 3 4]
+    ccall(obj_inspector, Cvoid, (Any,), a0)
+
+    s0 = S(1)
+    ccall(obj_inspector, Cvoid, (Any,), s0)
+    s1 = S(1)
+    ccall(obj_inspector, Cvoid, (Any,), s1)
+
+    d = 1
+    ccall(obj_inspector, Cvoid, (Any,), d)
+    d = 2
+    ccall(obj_inspector, Cvoid, (Any,), d)
+end
+
+
 const global_buf = Vector{UInt8}(undef, 16)
 for i in 1:16
     global_buf[i] = i
 end
 
-ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
-a1 = unsafe_wrap(Array, ptr, (2, 2))
-@show ptr, a1
-ccall(inspector, Ref{Array}, (Any, Ptr{Cvoid}), a1, C_NULL)
+function t2()
 
-ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 8
-a2 = unsafe_wrap(Array, ptr, (2, 2))
-@show ptr, a2
-ccall(inspector, Ref{Array}, (Any, Ptr{Cvoid}), a2, C_NULL)
+    ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
+    a1 = unsafe_wrap(Array, ptr, (2, 2))
+    @show ptr, a1
+    ccall(inspector, Ref{Array}, (Any, Ptr{Cvoid}), a1, C_NULL)
 
+    ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 8
+    a2 = unsafe_wrap(Array, ptr, (2, 2))
+    @show ptr, a2
+    ccall(inspector, Ref{Array}, (Any, Ptr{Cvoid}), a2, C_NULL)
 
-ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 8
-ccall(inspector, Ref{Array}, (Any, Ptr{Cvoid}), a1, ptr)
-ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
-ccall(inspector, Ref{Array}, (Any, Ptr{Cvoid}), a2, ptr)
+    ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 8
+    ccall(inspector, Ref{Array}, (Any, Ptr{Cvoid}), a1, ptr)
+    ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
+    ccall(inspector, Ref{Array}, (Any, Ptr{Cvoid}), a2, ptr)
 
-@show a1
-@show a2
+    @show a1
+    @show a2
+end
+
+struct B
+    i::Int
+    j::Int
+    k::Int
+    x::Int
+end
+
+function t(v)
+    ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
+    a1 = unsafe_wrap(Array, ptr, (2, 2))
+
+    println("Hi, I am from Julia")
+    println(v)
+    dest = Core.bitcast(Ptr{Cvoid}, v)
+    @show dest
+
+    a2 = ccall(a_copy, Any, (Any, Ptr{Cvoid}), a1, dest)
+    return a2
+end
+
+x = ccall(icaller, Any, (Any,), t)
+# print(x[1])
+# t(1)
