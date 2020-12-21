21dd09bd75bba54972ae1e00995d07ba517507b3
KDr2 <zhuo@hexoasis.com>
2020-12-21 08:02:38 +0000

    add some debugging code

--- src/mempool.jl
+++ src/mempool.jl
@@ -7,32 +7,54 @@ using Libdl
 @show call_with_stackbuf = dlsym(lib, :lt_call_with_stackbuf)
 @show copy_to_stack_array = dlsym(lib, :lt_copy_to_stack_array)
 
-# GC.enable(false)
 
-# global memory buffer
+a = [1 3; 2 4]
+
+# global memory buffer and const array
 const global_buf = Vector{UInt8}(undef, 16)
 for i in 1:16
     global_buf[i] = i
 end
 
+const global_array = [10 30; 20 40]
+
+function get_array(mempool=false)
+    if mempool
+        ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
+        # a1 is allocated on heap, both of the box-object and the data buffer
+        a1 = unsafe_wrap(Array{UInt8, 2}, ptr, (2, 2))
+        return a1
+    else
+        return global_array
+    end
+end
+
+function copy_to_stack(obj, dest; method=:noncopy) # :noncopy|:stkcopy|:weakref
+    method == :noncopy && return obj
+    o = ccall(copy_to_stack_array, Any, (Any, Ptr{Cvoid}), obj, dest)
+    method == :weakref && return WeakRef(o)
+    return o
+end
+
+stkobj(a) = a
+stkobj(a::WeakRef) = a.value
+
 # common test
 println("============= common test =")
-
 function entrance(stack_buffer)
     # my caller is `lt_call_with_stackbuf`, when I return,
     # the stack-allocated buffer will be released.
-
-    ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
-    # a1 is allocated on heap, both of the box-object and the data buffer
-    a1 = unsafe_wrap(Array{UInt8, 2}, ptr, (2, 2))
+    a1 = get_array()
     println("Hi, I am from Julia")
     println(typeof(a1))
     println(stack_buffer)
     dest = Core.bitcast(Ptr{Cvoid}, stack_buffer)
     @show dest
 
-    a2 = ccall(copy_to_stack_array, Any, (Any, Ptr{Cvoid}), a1, dest)
-    @show a2, a2[1], a2[2]
+    # ccall(copy_to_stack_array, Any, (Any, Ptr{Cvoid}), a1, dest)
+    a2 = copy_to_stack(a1, dest)
+    @show a2, stkobj(a2)[1], stkobj(a2).value[2]
+    GC.gc()
 end
 
 function test()
@@ -48,24 +70,22 @@ tr = Ref{Task}()
 t1 = @task for _ in 1:4
     @show "---"
     yieldto(tr[])
+    GC.gc()
 end
 
 function entrance_t(stack_buffer)
     # my caller is `lt_call_with_stackbuf`, when I return,
     # the stack-allocated buffer will be released.
-    ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
-    a1 = unsafe_wrap(Array{UInt8, 2}, ptr, (2, 2))
-    println("Hi, I am from Julia")
-    println(typeof(a1))
-    println(stack_buffer)
+
+    a1 = get_array()
     dest = Core.bitcast(Ptr{Cvoid}, stack_buffer)
-    @show dest
-    a2 = ccall(copy_to_stack_array, Any, (Any, Ptr{Cvoid}), a1, dest)
-    @show a2, a2[1], a2[2]
+    # @show dest
+    a2 = copy_to_stack(a1, dest)
+    @show a2, stkobj(a2)[1], stkobj(a2)[2]
     istaskdone(t1) || yieldto(t1)
-    @show a2, a2[1], a2[2]
+    @show a2, stkobj(a2)[1], stkobj(a2)[2]
     istaskdone(t1) || yieldto(t1)
-    @show a2, a2[1], a2[2]
+    @show a2, stkobj(a2)[1], stkobj(a2)[2]
     istaskdone(t1) || yieldto(t1)
 end
 
@@ -73,11 +93,11 @@ function test_t()
     ccall(call_with_stackbuf, Any, (Any, UInt), entrance_t, 1024)
 end
 
-t = @task test_t()
-tr[] = t
-schedule(t)
-wait(t)
-
+tr[] = @task test_t()
+schedule(tr[])
+wait(tr[])
+@show tr[].state
+GC.gc()
 
 
 # Libtask test
@@ -86,21 +106,13 @@ println("============= libtask test =")
 using Libtask
 
 function lt_e(stack_buffer)
-    ptr = unsafe_convert(Ptr{UInt8}, global_buf) + 0
-    a1 = unsafe_wrap(Array{UInt8, 2}, ptr, (2, 2))
-    println("Hi, I am from Julia")
-    println(typeof(a1))
-    println(stack_buffer)
+    a1 = get_array()
     dest = Core.bitcast(Ptr{Cvoid}, stack_buffer)
-    @show dest
-
-    a2 = ccall(copy_to_stack_array, Any, (Any, Ptr{Cvoid}), a1, dest)
-    # @show "something..." # why we must have this line?
-    # @show a2
+    a2 = copy_to_stack(a1, dest)
 
     t = 1
     while true
-        produce(a2[t])
+        produce(stkobj(a2)[t])
         t = 1 + t
     end
 end
@@ -115,8 +127,11 @@ ctask = CTask(lt_f)
 @show consume(ctask) # 1
 
 
-@show "gonna copy!" # And why this line?
+# @show "gonna copy!" # And why this line?
+GC.enable(false)
 a = copy(ctask)
+GC.enable(true)
 @show "after copy!"
+GC.gc()
 @show consume(a) # 2
 @show consume(a) # 3
