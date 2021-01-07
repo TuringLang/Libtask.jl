fd4abd6148bee0d9a30311e173f9a8a6da609872
KDr2 <zhuo@hexoasis.com>
2021-01-07 07:04:50 +0000

    update benchmarks

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -1,23 +1,32 @@
 using BenchmarkTools
 using Libtask
 
-INTENSITY = 3
+
+ macro rep(cnt, exp)
+     blk =:(begin end)
+     for _ in 1:eval(cnt)
+         push!(blk.args, esc(exp))
+     end
+     blk
+ end
+
+INTENSITY = 2
 
 println("= Benchmarks on Arrays =")
 A = rand(1000, 1000)
 x, y =  abs.(rand(Int, 2) .% 999) .+ 1
 print("indexing: ")
-@btime for _ in 1:INTENSITY; $A[$x, $y]; end
+@btime @rep INTENSITY $A[$x, $y]
 print("set indexing: ")
-@btime for _ in 1:INTENSITY; $A[$x, $y] = 1; end
+@btime @rep INTENSITY $A[$x, $y] = 1
 print("broadcast: ")
-@btime for _ in 1:INTENSITY; $A .+ $A; end
+@btime @rep INTENSITY $A .+ $A
 
 println("= Benchmarks on TArrays =")
 TA = Libtask.localize(deepcopy(A))
 print("indexing: ")
-@btime for _ in 1:INTENSITY; $TA[$x, $y]; end
+@btime @rep INTENSITY $TA[$x, $y]
 print("set indexing: ")
-@btime for _ in 1:INTENSITY; $TA[$x, $y] = 1; end
+@btime @rep INTENSITY $TA[$x, $y] = 1
 print("broadcast: ")
-@btime for _ in 1:INTENSITY; $TA .+ $TA; end
+@btime @rep INTENSITY $TA .+ $TA
