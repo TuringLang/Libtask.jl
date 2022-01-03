cf485df1ba503c2c8b767cbfbcca7f0ac064a322
Hong Ge <hg344@cam.ac.uk>
2022-01-03 21:34:01 +0000

    Update src/tapedtask.jl

--- src/tapedtask.jl
+++ src/tapedtask.jl
@@ -108,7 +108,6 @@ end
 end
 
 function produce(val)
-    # put!(ttask.produce_ch, val)
     # take!(ttask.consume_ch) # wait for next consumer
     is_in_tapedtask() || return nothing
     ttask = current_task().storage[:tapedtask]
