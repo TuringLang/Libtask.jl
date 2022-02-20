c0ffb30711aa4c5b7dba2f8c6766f227925d71e1
Hong Ge <yebai@users.noreply.github.com>
2022-02-20 20:42:15 +0000

    Update tf.jl

--- test/tf.jl
+++ test/tf.jl
@@ -2,7 +2,7 @@ using Libtask
 
 @testset "tapedfunction" begin
     # Test case 1: stack allocated objects are deep copied.
-    @testset "Instruction{typeof(_new)}" begin
+    @testset "Instruction{typeof(__new__)}" begin
         mutable struct S
             i::Int
             S(x, y) = new(x + y)
@@ -11,7 +11,7 @@ using Libtask
         tf = Libtask.TapedFunction(S, 1, 2)
         s1 = tf(1, 2)
         @test s1.i == 3
-        newins = findall(x -> isa(x, Libtask.Instruction{typeof(Libtask._new)}), tf.tape)
+        newins = findall(x -> isa(x, Libtask.Instruction{typeof(Libtask.__new__)}), tf.tape)
         @test length(newins) == 1
     end
 end
