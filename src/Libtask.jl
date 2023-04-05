9befa393108a70e33018ddfdc257f4d8da56b3a5
Hong Ge <3279477+yebai@users.noreply.github.com>
2023-04-05 13:07:21 +0100

    Update src/Libtask.jl
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/Libtask.jl
+++ src/Libtask.jl
@@ -8,7 +8,7 @@ export TapedTask, consume, produce
 export TArray, tzeros, tfill, TRef # legacy types back compat
 
 
-TypedSlot = @static if isdefined(Core, :TypedSlot) # Julia v1.10 removed Core.TypedSlot
+const TypedSlot = @static if isdefined(Core, :TypedSlot) # Julia v1.10 removed Core.TypedSlot
     Core.TypedSlot
 else
     Core.Compiler.TypedSlot
