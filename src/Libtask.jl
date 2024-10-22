92b3a6d70b04313cf69e8f1cc9bf43d6f57b928f
KDr2 <zhuo.dev@gmail.com>
2024-10-22 08:59:27 +0800

    comment about TypedSlot

--- src/Libtask.jl
+++ src/Libtask.jl
@@ -10,6 +10,7 @@ export TArray, tzeros, tfill, TRef # legacy types back compat
 
 @static if isdefined(Core, :TypedSlot) || isdefined(Core.Compiler, :TypedSlot)
     # Julia v1.10 removed Core.TypedSlot
+    # Julia v1.11 removed Core.Compiler.TypedSlot
     const TypedSlot = @static if isdefined(Core, :TypedSlot)
         Core.TypedSlot
     else
