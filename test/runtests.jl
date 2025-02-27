61594e4bf50e210c6dd2f788f103d186b6fc5901
willtebbutt <wtebbutt@turing.ac.uk>
2025-02-27 16:30:01 +0000

    Update project deps etc

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,10 @@
 using JuliaFormatter, Libtask, Test
 
 @testset "Libtask" begin
-
+    @testset "quality" begin
+        Aqua.test_all(Libtask)
+        @test JuliaFormatter.format(Mooncake; verbose=false, overwrite=false)
+    end
     include("copyable_task.jl")
     include("issues.jl")
 end
