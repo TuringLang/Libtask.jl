a63660e621f921ecdec9a80808a265f436d3d418
willtebbutt <wtebbutt@turing.ac.uk>
2025-03-07 16:53:44 +0000

    Do not check for stale deps on 1.11

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
 include("front_matter.jl")
 @testset "Libtask" begin
     @testset "quality" begin
-        Aqua.test_all(Libtask)
+        # ScopedValues is stale on 1.11.
+        Aqua.test_all(Libtask; stale_deps=VERSION < v"1.11" ? true : false)
         @test JuliaFormatter.format(Libtask; verbose=false, overwrite=false)
     end
     include("copyable_task.jl")
