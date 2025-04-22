208674820da4add6f41ce90fc35d9175f54983f4
Will Tebbutt <3628294+willtebbutt@users.noreply.github.com>
2025-04-22 15:47:22 +0100

    Sort out consume

--- src/copyable_task.jl
+++ src/copyable_task.jl
@@ -29,8 +29,8 @@ __v::Int = 5
 """
     produce(x)
 
-When run inside a [`TapedTask`](@ref), will immediately yield to the caller, producing value
-`x`.
+When run inside a [`TapedTask`](@ref), will immediately yield to the caller, returning value
+`x`. Users will typically hit this function when calling `consume`.
 
 See also: [`Libtask.consume`](@ref)
 """
