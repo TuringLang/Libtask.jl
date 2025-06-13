89b4d8083b732d2b9684cf2c9b96fbdee71d2b36
Markus Hauru <mhauru@turing.ac.uk>
2025-06-13 18:16:12 +0100

    Add helpful error for naked produce call

--- src/copyable_task.jl
+++ src/copyable_task.jl
@@ -72,6 +72,13 @@ the current world age, will make a copy of an existing `MistyClosure`. If not,
 will derive it from scratch (derive the IR + compile it etc).
 """
 function build_callable(sig::Type{<:Tuple})
+    if sig <: Tuple{typeof(produce),Any}
+        msg = """
+            Can not construct a TapedTask for a 'naked' call to `produce`.
+             Please wrap the call to `produce` in a function, and construct a
+             TapedTask from that function."""
+        throw(ArgumentError(msg))
+    end
     key = CacheKey(Base.get_world_counter(), sig)
     if haskey(mc_cache, key)
         return fresh_copy(mc_cache[key])
